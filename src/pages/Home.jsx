70e0336b1b484b0d095156149622d26387d17f9e
Hamed Jahangiry <jahangiryh73@gmail.com>
2025-07-03 20:30:56 +0330

    Initial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+function Home() {
+  return (
+    <>
+      <header>
+        <div>
+          <a href="">Hamed jahangiry</a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">section 1</a>
+            </li>
+            <li>
+              <a href="#">section 2</a>
+            </li>
+            <li>
+              <a href="#">section 2</a>
+            </li>
+          </ul>
+        </nav>
+        <button></button>
+      </header>
+    </>
+  );
+}
+
+export default Home;
