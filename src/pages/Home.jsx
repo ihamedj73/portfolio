4c9037597aaf7eb9f37e4f6fa3cda1dab1421a02
Hamed Jahangiry <jahangiryh73@gmail.com>
2025-07-04 17:17:52 +0330

    Inital

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,9 @@
+import HeaderNav from "../UI/HeaderNav";
+
 function Home() {
   return (
     <>
-      <header>
-        <div>
-          <a href="">Hamed jahangiry</a>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href="#">section 1</a>
-            </li>
-            <li>
-              <a href="#">section 2</a>
-            </li>
-            <li>
-              <a href="#">section 2</a>
-            </li>
-          </ul>
-        </nav>
-        <button></button>
-      </header>
+      <HeaderNav />
     </>
   );
 }
